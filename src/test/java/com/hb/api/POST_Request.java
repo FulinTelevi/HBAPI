c43339c1683a26b9c766e4dc20661cd9e46ba84d
[Fulin] <[fulintelevi@gmail.com]>
2024-04-30 13:01:31 +0300

    method eklendi

--- src/test/java/com/hb/api/POST_Request.java
+++ src/test/java/com/hb/api/POST_Request.java
@@ -1,5 +1,7 @@
 package com.hb.api;
 
+import java.util.Random;
+
 import org.junit.Test;
 
 import com.google.gson.Gson;
@@ -15,23 +17,28 @@ public class POST_Request {
 		RestAssured.baseURI = "http://generator.swagger.io";
 		String getStatusEndPoint = "/api/gen/clients";
 		Response response = RestAssured.when().post(getStatusEndPoint);
-		String body =RestAssured.given().when().get(getStatusEndPoint).getBody().asString();
+		String body = RestAssured.given().when().get(getStatusEndPoint).getBody().asString();
 		String[] bodyArray = new Gson().fromJson(body, String[].class);
-		String postStatusEndPoint = "/api/gen/clients/" + bodyArray[0];
-		System.out.println(postStatusEndPoint);
-		String payload = "{\r\n" + "  \"spec\": \"\"}";
-		
 
-		RestAssured.given()
-		.header("Content-Type", "application/json")
-		.body(payload)
-		.when()
-		.post(postStatusEndPoint)
-		.then()
-		.assertThat()
-		.statusCode(200);
+		String randomElement = getRandomElement(bodyArray);
+		System.out.println(randomElement);
+
+		String postStatusEndPoint = "/api/gen/clients/" + randomElement;
+		System.out.println(postStatusEndPoint);
+
+		String payload = "{\r\n" + "  \"spec\": \"\"}";
+
+		System.out.println(response.getStatusCode());
+
+		RestAssured.given().header("Content-Type", "application/json").body(payload).when().post(postStatusEndPoint)
+				.then().assertThat().statusCode(200);
 
 	}
 
+	public static String getRandomElement(String[] array) {
+		Random rand = new Random();
+		int randomIndex = rand.nextInt(array.length);
+		return array[randomIndex];
+	}
 
 }
